824d30161b75762d03bb7175008e2ce790c10905
Roman Makeev <makeevrserg@gmail.com>
2021-04-06 19:21:38 +0300

    -result

--- app/src/main/java/com/makeevrserg/musicplayer/MainActivity.kt
+++ app/src/main/java/com/makeevrserg/musicplayer/MainActivity.kt
@@ -1,59 +1,63 @@
 package com.makeevrserg.musicplayer
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.media.MediaMetadataRetriever
 import android.media.MediaPlayer
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.provider.MediaStore
-import android.util.Log
 import android.webkit.MimeTypeMap
 import android.widget.SeekBar
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_debug.*
-import java.lang.Exception
-import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+    private val TAG: String = "MainActivity"
+    private val MUSIC_RESULT1: Int = 10
+    private val MUSIC_RESULT2: Int = 11
+
+    private var mContext: Context? = null
+    private var crossfade = 2
+
     private var mediaPlayer1: MediaPlayer? = null
     private var mediaPlayer2: MediaPlayer? = null
     private var currentMediaPlayer: MediaPlayer? = null
 
-    private final val TAG: String = "MainActivity"
-    private final val MUSIC_RESULT1: Int = 10
-    private final val MUSIC_RESULT2: Int = 11
-    private final var mContext: Context? = null
-    private var crossfade = 2
+    private var toast: Toast? = null
 
+    //Должен быть глобальной переменной чтобы сбросить всё, если сменится основная музыка
+    var handler: Handler? = null
 
-    @SuppressLint("UseCompatLoadingForDrawables")
+    fun CheckToast() {
+        if (toast == null)
+            return
+        toast!!.cancel()
+        toast = null
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mContext = applicationContext
         setContentView(R.layout.activity_debug)
-        buttonMusic1.setOnClickListener {
-            SetMusicIntent(MUSIC_RESULT1)
-        }
-        buttonMusic2.setOnClickListener {
-            SetMusicIntent(MUSIC_RESULT2)
-        }
+
+        mContext = applicationContext
+
+        buttonMusic1.setOnClickListener { SetMusicIntent(MUSIC_RESULT1) }
+        buttonMusic2.setOnClickListener { SetMusicIntent(MUSIC_RESULT2) }
+
         seekBarCrossfade.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 //Нельзя менять во время проигрывания
                 if (currentMediaPlayer != null && currentMediaPlayer!!.isPlaying) {
-                    Toast.makeText(
+                    CheckToast()
+                    toast = Toast.makeText(
                         mContext,
                         "Нельзя менять во время проигрывания",
                         Toast.LENGTH_SHORT
                     )
-                        .show()
+                    toast!!.show()
+                    //Ставим старое значение
                     seekBar!!.progress = crossfade
                 } else
                     crossfade = progress
@@ -71,16 +75,34 @@ class MainActivity : AppCompatActivity() {
         })
 
         imageButtonPause.setOnClickListener {
+
             if (mediaPlayer1 == null || mediaPlayer2 == null) {
-                Toast.makeText(this, "Не выбран один из музыкальных файлов", Toast.LENGTH_SHORT)
-                    .show()
+                CheckToast()
+                toast =
+                    Toast.makeText(this, "Не выбран один из музыкальных файлов", Toast.LENGTH_SHORT)
+                toast!!.show()
                 return@setOnClickListener
             }
+
             if (currentMediaPlayer == null) {
                 currentMediaPlayer = mediaPlayer1
-                initializeSeekBar()
+                initDelay()
             }
-
+            //Нельзя начать играть если длина файлов меньше кроссфейда
+            if (mediaPlayer1!!.duration / 1000 < crossfade || mediaPlayer2!!.duration / 1000 < crossfade) {
+                CheckToast()
+                toast = Toast.makeText(
+                    this,
+                    "Один из аудиофайлов слишком короткий для данного кроссфейда",
+                    Toast.LENGTH_SHORT
+                )
+                toast!!.show()
+                if (mediaPlayer2!!.isPlaying)
+                    mediaPlayer2!!.pause()
+                if (mediaPlayer1!!.isPlaying)
+                    mediaPlayer1!!.pause()
+                imageButtonPause.setImageDrawable(getDrawable(R.drawable.ic_play))
+            }
             if (currentMediaPlayer!!.isPlaying) {
                 imageButtonPause.setImageDrawable(getDrawable(R.drawable.ic_play))
                 currentMediaPlayer!!.pause()
@@ -91,28 +113,9 @@ class MainActivity : AppCompatActivity() {
                 currentMediaPlayer!!.start()
             }
         }
-
-
-        //Debug only
-        seekBarProgress.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                if (fromUser) currentMediaPlayer?.seekTo(progress)
-            }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                return
-            }
-
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                return
-            }
-
-        })
-
-
     }
 
-    //Запускаем интент который покажет список наших mp3 файлов
+    //Задаем интент который покажет список наших mp3 файлов
     //Запускаем активити которая возвратит результат
     fun SetMusicIntent(RES: Int) {
         val fileBrowserIntent: Intent =
@@ -122,80 +125,73 @@ class MainActivity : AppCompatActivity() {
         startActivityForResult(fileBrowserIntent, RES)
     }
 
-    @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        //По requestCod'у видим наши активити
         when (requestCode) {
             MUSIC_RESULT1 ->
-                if (resultCode == Activity.RESULT_OK) {
+                if (resultCode == Activity.RESULT_OK) {//Проверям что активити возвращена с результатом
                     if (mediaPlayer1 != null) {
+                        //Удаляем у хендлера все его postDelayed'ы
+                        if (handler != null)
+                            handler!!.removeCallbacksAndMessages(null)
+                        //Ресетим активный плеер
                         if (currentMediaPlayer != null && currentMediaPlayer == mediaPlayer1) {
                             currentMediaPlayer!!.reset()
                             currentMediaPlayer = null
                             imageButtonPause.setImageDrawable(getDrawable(R.drawable.ic_play))
                         }
                         mediaPlayer1!!.reset()
+                        mediaPlayer2!!.pause()
                     }
                     mediaPlayer1 = MediaPlayer.create(this, data?.data!!)
                 }
+
             MUSIC_RESULT2 ->
                 if (resultCode == Activity.RESULT_OK) {
                     if (mediaPlayer2 != null)
                         mediaPlayer2!!.reset()
                     mediaPlayer2 = MediaPlayer.create(this, data?.data!!)
-                    Log.d(TAG, "onActivityResult: Created Media2")
                 }
 
         }
     }
 
-    //Debug only
-    private fun toMim(seconds: Int): String {
-        return (seconds / 60).toString() + ":" + (seconds % 60).toString()
-    }
-
-    private fun initializeSeekBar() {
-        //Debug only
-        seekBarProgress.max = currentMediaPlayer!!.duration
-        //Debug only
-        textViewMaxProgress.setText(toMim(currentMediaPlayer!!.duration / 1000))
-
-        val handler = Handler()
-        handler.postDelayed(object : Runnable {
+    private fun initDelay() {
+        handler = Handler()
+        handler!!.postDelayed(object : Runnable {
             override fun run() {
-                try {
-                    //Debug only
-                    seekBarProgress.progress = currentMediaPlayer!!.currentPosition
-                    val toEnd =
-                        (currentMediaPlayer!!.duration - currentMediaPlayer!!.currentPosition) / 1000
-                    //Делаем кроссфейд
-                    if (mediaPlayer2!!.isPlaying && toEnd > crossfade)
-                        mediaPlayer2!!.pause()
-                    if (toEnd <= crossfade && currentMediaPlayer!!.isPlaying) {
-                        if (!mediaPlayer2!!.isPlaying) {
-                            mediaPlayer2!!.start()
-                            Log.d(TAG, "seekTo: " + (crossfade - toEnd) * 1000)
-                            Log.d(TAG, "currPos: " + mediaPlayer2!!.currentPosition)
-                            if (mediaPlayer2 == null)
-                                Log.d(TAG, "null: ")
-                            mediaPlayer2!!.seekTo((crossfade - toEnd) * 1000)
-                        }
-                        Log.d(TAG, "media2Voilume: " + 1.0f / toEnd)
-                        mediaPlayer2!!.setVolume(1.0f / toEnd, 1.0f / toEnd)
+
+                val toEnd =
+                    (currentMediaPlayer!!.duration - currentMediaPlayer!!.currentPosition) / 1000//Переводим время до конца в секунды
+                //Если мы поставили на паузу, а потом сменили длительность кроссфейда - надо проверить, активировать ли второе аудио или поставить на паузу
+                if (mediaPlayer2!!.isPlaying && toEnd > crossfade)
+                    mediaPlayer2!!.pause()
+                //Если попали в зону кроссфейда и основная запись не на паузе
+                if (toEnd <= crossfade && currentMediaPlayer!!.isPlaying) {
+                    //Включаем вторую запись
+                    if (!mediaPlayer2!!.isPlaying) {
+                        mediaPlayer2!!.start()
+                        //Если мы сменили кроссфейд после паузу - должны перемотать вторую запись на новый момент
+                        mediaPlayer2!!.seekTo((crossfade - toEnd) * 1000)
                     }
-                    if (toEnd == 0) {
-                        currentMediaPlayer = mediaPlayer2
-                        mediaPlayer2 = mediaPlayer1
-                        mediaPlayer1 = currentMediaPlayer
-                        initializeSeekBar()
-                    }
-                    textViewCurrentProgress.setText(toMim(currentMediaPlayer!!.currentPosition / 1000))
-                    if (toEnd >= 1)
-                        handler.postDelayed(this, 1000)
-                } catch (e: Exception) {
-                    seekBarProgress.progress = 0
+                    //Ставим громкость звука с delay'м в 100мс
+                    val mSound: Float = (1.0f - (toEnd.toFloat() / (crossfade)))
+                    mediaPlayer2!!.setVolume(mSound, mSound)
                 }
+                //Когда заканчивается первая запись - меняем указатели друг на друга
+                //Теперь основной плеер у нас - это второй, а первый становится вторым
+                if (toEnd == 0) {
+                    currentMediaPlayer = mediaPlayer2
+                    mediaPlayer2 = mediaPlayer1
+                    mediaPlayer1 = currentMediaPlayer
+                    initDelay()
+                }
+                //Если основнеая запись не окончена - запускаем эту же функцию снова
+                if (toEnd >= 1)
+                    handler!!.postDelayed(this, 200)
+
             }
 
         }, 0)
